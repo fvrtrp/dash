16c2f65facf58b13e5f27cf85d5a97909e65c5e0
surajk95 <surajkukati95@gmail.com>
2022-12-22 00:33:56 +0530

    init tasks

--- popup.js
+++ popup.js
@@ -1,87 +1,78 @@
-// Initialize button with user's preferred color
+let tasks = [], config = {}
 let userInput = document.querySelector("#userInput")
 const resultContainer = document.querySelector("#result")
 const errorContainer = document.querySelector("#error")
 
-userInput.addEventListener('input', (event) => {
-    //fetch thumbnail
-    const imageParents = document.querySelectorAll(".imageParent")
-    imageParents.forEach(i => {i.remove()})
-    const result = fetchImage(event.target.value.trim())
-    if(result.error) {
-        errorContainer.innerHTML = result.error
-        errorContainer.style.display = 'block'
-        resultContainer.style.display = 'none'
-    }
-    else {
-        errorContainer.style.display = 'none'
-        resultContainer.style.display = 'flex'
-        let imgContainer = null, imgParent = null, imgLink = null, imgDesc = null
-        if(result.maxres) {
-            imgParent = document.createElement('div')
-            imgParent.classList.add("imageParent")
-            imgDesc = document.createElement('div')
-            imgDesc.innerHTML = 'highest quality'
-            imgParent.appendChild(imgDesc)
-            imgLink = document.createElement('a')
-            imgLink.href = result.maxres
-            imgLink.setAttribute('target', '_blank')
-            imgContainer = document.createElement("img")
-            imgContainer.src = result.maxres
-            imgContainer.classList.add("imageContainer")
-            imgLink.appendChild(imgContainer)
-            imgParent.appendChild(imgLink)
-            resultContainer.appendChild(imgParent)
-        }
-        if(result.hq) {
-            imgParent = document.createElement('div')
-            imgParent.classList.add("imageParent")
-            imgDesc = document.createElement('div')
-            imgDesc.innerHTML = 'medium quality'
-            imgParent.appendChild(imgDesc)
-            imgLink = document.createElement('a')
-            imgLink.href = result.hq
-            imgLink.setAttribute('target', '_blank')
-            imgContainer = document.createElement("img")
-            imgContainer.src = result.hq
-            imgContainer.classList.add("imageContainer")
-            imgLink.appendChild(imgContainer)
-            imgParent.appendChild(imgLink)
-            resultContainer.appendChild(imgParent)
-        }
-        if(result.mq) {
-            imgParent = document.createElement('div')
-            imgParent.classList.add("imageParent")
-            imgDesc = document.createElement('div')
-            imgDesc.innerHTML = 'lowest quality'
-            imgParent.appendChild(imgDesc)
-            imgLink = document.createElement('a')
-            imgLink.href = result.mq
-            imgLink.setAttribute('target', '_blank')
-            imgContainer = document.createElement("img")
-            imgContainer.src = result.mq
-            imgContainer.classList.add("imageContainer")
-            imgLink.appendChild(imgContainer)
-            imgParent.appendChild(imgLink)
-            resultContainer.appendChild(imgParent)
-        }
+window.onload = load_data()
+
+userInput.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+        add_task()
     }
 })
 
-const fetchImage = (url) => {
-    try {
-        let aaa = /(.*)(com|be)\/(watch\?v=)?(.{11})[?|&]?.*/g;
-        let imgUrl = aaa.exec(url)[4];
-        let newmq="https://i.ytimg.com/vi/"+imgUrl+"/mqdefault.jpg";
-        let newhq="https://i.ytimg.com/vi/"+imgUrl+"/hqdefault.jpg";
-        let newmaxres="https://i.ytimg.com/vi/"+imgUrl+"/maxresdefault.jpg";
-        return({
-            mq: newmq,
-            hq: newhq,
-            maxres: newmaxres
-        })
+function load_data() {
+    storage('read')
+}
+
+function add_task() {
+    const input = document.querySelector('#userInput')
+    const val = input.value
+    if(!val) val = ''
+    const task = {
+        val: val,
+        id: tasks.length===0 ? 0 : tasks[tasks.length-1]['id']+1
     }
-    catch {
-    return ({error: `Link is not valid, please try with a different one.`})
+    tasks.push(task)
+    storage('update', {config: config, tasks: tasks})
+    input.value = ''
+    renderTask(task)
+}
+
+
+
+function storage(action, data) {
+    switch(action) {
+        case 'read': {
+            chrome.storage.local.get(['dash-todo'], function(data) {
+                if(!data || Object.keys(data).length===0) data = {
+                    tasks: tasks, config: config
+                }
+                else {
+                    data = data['dash-todo']
+                }
+                after_load(data)
+            })
+            break
+        }
+        case 'update': {
+            chrome.storage.local.set({'dash-todo': data}, function() {
+                console.log('Value is set to ' + data)
+            })
+            break
+        }
     }
-  }
+}
+
+function after_load(data) {
+    tasks = data['tasks']
+    config = data['config']
+    renderTasks(tasks)
+}
+
+function renderTasks(tasks) {
+    for(let task of tasks)
+        renderTask(task)
+}
+
+function renderTask(task) {
+    const el = document.createElement('div')
+    el.id = `task-${task.id}`
+    el.className = 'taskItem'
+    const title = document.createElement('div')
+    title.innerHTML = task.val
+    el.appendChild(title)
+
+    const container  = document.querySelector('#tasks')
+    container.appendChild(el)
+}
